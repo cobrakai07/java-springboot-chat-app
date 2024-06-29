3c7585002e46793d3fbea109df9b298f56c8e62f
Kai <dk786161616@gmail.com>
2024-06-29 11:28:58 +0530

    started chat app

---
+++ src/test/java/com/deepak/groupchat/GroupchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.groupchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
